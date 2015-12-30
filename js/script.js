8e9481406b1025938378cf2454241c9cc89b7b02
Steven Marturano <steven.marturano@gmail.com>
2015-12-30 02:22:26 -0500

    changed frontend to landscape layout

--- js/script.js
+++ js/script.js
@@ -1,8 +1,8 @@
 
 
 
-var c = document.getElementById("canvas1");
-var ctx = c.getContext("2d");
+var c1 = document.getElementById("canvas1");
+var c1_context = c1.getContext("2d");
 ctx.beginPath();
 ctx.arc(200,50,40,0,2*Math.PI);
 ctx.stroke();
