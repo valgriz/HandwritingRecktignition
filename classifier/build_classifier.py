e398cbd928880f5eb4c8aea79c87a70aae50ebe7
Josh <j.adam.downes@gmail.com>
2015-12-31 16:06:13 -0500

    cgi

--- classifier/build_classifier.py
+++ classifier/build_classifier.py
@@ -1,42 +1,51 @@
+#!/usr/bin/python
+
 import numpy as np
 import cv2
+import cgi, cgitb
 from matplotlib import pyplot as plt
 
-def index(req):
-    # digits.png is an opencv image that contains 20x20 pixel handwritten digits
-    img = cv2.imread('digits.png')
-    gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+form = cgi.FieldStorage()
 
-    # split the image to 5000 cells, each 20x20 size
-    cells = [np.hsplit(row,100) for row in np.vsplit(gray,50)]
+canvas = form.getvalue('canvas')
 
-    # Make it into a Numpy array. Its size will be (50,100,20,20)
-    x = np.array(cells)
 
-    # Now we prepare train_data
-    train = x.reshape(-1,400).astype(np.float32) # Size = (2500,400)
+print "Content-type:text/html\r\n\r\n"
 
-    # Create labels for train and test data
-    k = np.arange(10)
-    train_labels = np.repeat(k,500)[:,np.newaxis]
+# digits.png is an opencv image that contains 20x20 pixel handwritten digits
+img = cv2.imread('digits.png')
+gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
 
-    # Initiate kNN, train the data, then test it with test data for k=1
-    knn = cv2.KNearest()
-    knn.train(train,train_labels)
+# split the image to 5000 cells, each 20x20 size
+cells = [np.hsplit(row,100) for row in np.vsplit(gray,50)]
 
-    # Load image we are testing
-    test_img = cv2.imread('test.png')
-    grey = cv2.cvtColor(test_img, cv2.COLOR_BGR2GRAY)
+# Make it into a Numpy array. Its size will be (50,100,20,20)
+x = np.array(cells)
 
-    # Create numpy array
-    nu_cells = [np.hsplit(row,20) for row in np.vsplit(grey, 20)]
-    nu_grey = np.array(nu_cells)
+# Now we prepare train_data
+train = x.reshape(-1,400).astype(np.float32) # Size = (2500,400)
 
-    nu_test = nu_grey.reshape(-1, 400).astype(np.float32)
+# Create labels for train and test data
+k = np.arange(10)
+train_labels = np.repeat(k,500)[:,np.newaxis]
 
-    # Use classifier 
-    ret2, result2, neighbours2, dist2 = knn.find_nearest(nu_test,k=1)
+# Initiate kNN, train the data, then test it with test data for k=1
+knn = cv2.KNearest()
+knn.train(train,train_labels)
 
-    print result2[0][0]
+# Load image we are testing
+test_img = cv2.imread('test.png')
+grey = cv2.cvtColor(test_img, cv2.COLOR_BGR2GRAY)
 
-    return 'OK'
+# Create numpy array
+nu_cells = [np.hsplit(row,20) for row in np.vsplit(grey, 20)]
+nu_grey = np.array(nu_cells)
+
+nu_test = nu_grey.reshape(-1, 400).astype(np.float32)
+
+# Use classifier 
+ret2, result2, neighbours2, dist2 = knn.find_nearest(nu_test,k=1)
+
+print result2[0][0]
+
+return 'OK'
